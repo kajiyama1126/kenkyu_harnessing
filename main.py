2ffce7502102629a5d74b92e78a34ff71058815f
kajiyama1126 <yuichi11261126@gmail.com>
2017-09-15 01:21:38 +0900

    0914 バグ修正

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import numpy as np
 from iteration import new_iteration_L2_harnessing_code,new_iteration_L2_harnessing_x_code
 
 if __name__ == '__main__':
-    n = 50
+    n = 20
     m = 20
-    lamb = 0.1
+    lamb = 0.0
     R = 400
     np.random.seed(0)  # ランダム値固定
     pattern = 2
-    test = 5000
+    test = 2000
     # step = [0.25, 0.25, 0.5, 0.5, 1., 1., 2., 2.]
     # step = [2.0,0.2,5.0,0.5,10.0,1.0,20.0,2.0]
     # step = [0.5, 0.5, 0.5, 0.7, 0.5, 0.9, 0.5, 0.99]
@@ -24,6 +24,6 @@ if __name__ == '__main__':
         # tmp = new_iteration_Dist(n, m, step, lamb, R, pattern, test)
         # tmp = new_iteration_L1_paper2(n, m, step, lamb, R, pattern, test)
         # tmp = new_iteration_L2_harnessing(n, m, step, lamb, R, pattern, test)
-        tmp = new_iteration_L2_harnessing_x_code(n, m, step, lamb, R, pattern, test)
+        tmp = new_iteration_L2_harnessing_code(n, m, step, lamb, R, pattern, test)
 
     print('finish2')
